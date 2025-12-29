8101ae00933a56950b38a8fa78e33ba7c2adc44f
thanhite7 <thanhnguyentienusd@gmail.com>
2025-12-29 11:23:17 +0700

    Update nginx proxy

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
   app.enableCors();
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   console.log(`Application is running on: http://localhost:${port}`);
 }
